cddeee97a69c7e888d4a5055cce8b4fd05a94496
NigelTanYong <nigel.ty28@gmail.com>
2024-03-02 11:49:31 +0800

    added comments

--- Swift/Mutithreading in Swift/Mutithreading in Swift/ViewController.swift
+++ Swift/Mutithreading in Swift/Mutithreading in Swift/ViewController.swift
@@ -16,11 +16,16 @@ class ViewController: UIViewController {
     }
 
     @IBAction func randomImage(_ sender: Any) {
+        // Perform task in a background thread to avoid blocking the main UI thread
         DispatchQueue.global(qos: .background).async{
+            // Construct URL for a random image
             let url = URL(string: "https://loremflickr.com/2000/2000")
+            // Fetch image data from the URL
             let data = try! Data(contentsOf: url!)
+            // Convert fetched data into UIImage
             let image = UIImage(data: data)
             
+            // Update UI on the main thread
             DispatchQueue.main.async{
                 self.imageView.image=image
             }
