8c0f347881fc85d6d495d5cb21242b194229becf
NigelTanYong <nigel.ty28@gmail.com>
2024-03-02 11:52:48 +0800

    added comments

--- Swift/Mutithreading in Swift/Mutithreading in Swift/ViewController.swift
+++ Swift/Mutithreading in Swift/Mutithreading in Swift/ViewController.swift
@@ -16,16 +16,21 @@ class ViewController: UIViewController {
     }
 
     @IBAction func randomImage(_ sender: Any) {
+        // Construct URL for a random image
         let url = URL(string: "https://loremflickr.com/2000/2000")!
+        // Create a download task with the shared URLSession
         let task = URLSession.shared.downloadTask(with:url){(localUrl, response, error) in
-            
+            // Read data from the downloaded file
             let data = try! Data(contentsOf: localUrl!)
+            // Convert fetched data into UIImage
             let image = UIImage(data: data)
             
+            // Update UI on the main thread
             DispatchQueue.main.async {
                 self.imageView.image=image
             }
         }
+        // Start the download task
         task.resume()
        
     }
